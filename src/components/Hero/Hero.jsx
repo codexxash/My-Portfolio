21c7e888cfafa3a0eb6510be5a942d58e45c7803
Ashutosh <ashusighrajput88@gmail.com>
2025-02-20 13:50:42 +0530

    some changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,8 +10,8 @@ function Hero() {
           <h1>Hi, I am <span className='myself'>Ashutosh Singh.</span> </h1>
           <h2>Aspiring Frontend Developer</h2>
         </div>
-        <div className="connects">
-          <span> <a href="https://drive.google.com/file/d/1T9u_RjB7KgluMipdFdSwznZbF3nZ5JDG/view?usp=drivesdk">resume</a></span>
+        <div className="resume">
+          <span> <a href="https://drive.google.com/file/d/1T9u_RjB7KgluMipdFdSwznZbF3nZ5JDG/view?usp=drivesdk">Resume</a></span>
 
         </div>
       </div>
